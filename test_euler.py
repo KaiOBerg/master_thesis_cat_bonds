d02d3cd1fd37f79c0cdb05775af23ac2a7ecfbcf
KaiOBerg <96840853+KaiOBerg@users.noreply.github.com>
2024-11-18 17:48:48 +0100

    m

--- test_euler.py
+++ test_euler.py
@@ -1,8 +1,11 @@
 import exposure_euler as ex_eu
 
+print('Recognized')
 if __name__ == "__main__":
     # Define parameters
+    print('Started')
     country = "308"
 
     # Call the function
     exp, applicable_basin, grid_gdf, islands_split_gdf, storm_basin_sub, tc_storms = ex_eu.init_TC_exp(country)
+    print('Job done')
