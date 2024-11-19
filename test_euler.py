cc9ed2cb1a5db74b9c3dce84add48f3839d51c10
KaiOBerg <96840853+KaiOBerg@users.noreply.github.com>
2024-11-19 09:28:41 +0100

    parallel euler

--- test_euler.py
+++ test_euler.py
@@ -1,11 +1,19 @@
 import exposure_euler as ex_eu
+from concurrent.futures import ProcessPoolExecutor
 
 print('Recognized')
+
+def process_country(cty):
+    """Wrapper function to process a single country."""
+    return ex_eu.init_TC_exp(country=cty)
+
 if __name__ == "__main__":
     # Define parameters
     print('Started')
-    countries = [242, 388, 192, 626]
+    countries = [242, 388, 192, 626]  # List of country codes
 
-    for cty in countries:
-        exp, applicable_basin, grid_gdf, islands_split_gdf, storm_basin_sub, tc_storms = ex_eu.init_TC_exp(country=cty)
+    # Use ProcessPoolExecutor to parallelize across countries
+    with ProcessPoolExecutor() as executor:
+        results = list(executor.map(process_country, countries))
+
     print('Job done')
