de060272337173daadb944d7beab41b336eb875e
KaiOBerg <96840853+KaiOBerg@users.noreply.github.com>
2024-11-25 22:08:59 +0100

    load files true

--- country_basics_cc_euler.py
+++ country_basics_cc_euler.py
@@ -7,7 +7,7 @@ import exposure_cc_euler as ex_cc_eu
 def process_country(cty, cc_model):
     """Wrapper function to process a single country."""
     print(f"Processing country: {cty}")
-    ex_cc_eu.init_TC_exp(country=cty, cc_model=cc_model, buffer_distance_km=105, res_exp=30, grid_size=6000, buffer_grid_size=1, load_fls=False, plot_exp=False, plot_centrs=False, plt_grd=False)
+    ex_cc_eu.init_TC_exp(country=cty, cc_model=cc_model, buffer_distance_km=105, res_exp=30, grid_size=6000, buffer_grid_size=1, load_fls=True, plot_exp=False, plot_centrs=False, plt_grd=False)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
