1c21cf2e6ec989524a0f5130717fc3cefaf76dad
KaiOBerg <96840853+KaiOBerg@users.noreply.github.com>
2024-11-25 20:18:20 +0100

    fixed function

--- country_basics_cc_euler.py
+++ country_basics_cc_euler.py
@@ -7,7 +7,7 @@ import exposure_cc_euler as ex_cc_eu
 def process_country(cty):
     """Wrapper function to process a single country."""
     print(f"Processing country: {cty}")
-    ex_cc_eu.init_TC_exp(country=country, cc_model='CMCC', buffer_distance_km=105, res_exp=30, grid_size=6000, buffer_grid_size=1, load_fls=False, plot_exp=False, plot_centrs=False, plt_grd=False)
+    ex_cc_eu.init_TC_exp(country=cty, cc_model='CMCC', buffer_distance_km=105, res_exp=30, grid_size=6000, buffer_grid_size=1, load_fls=False, plot_exp=False, plot_centrs=False, plt_grd=False)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -15,5 +15,5 @@ if __name__ == "__main__":
         sys.exit(1)
     
     country = int(sys.argv[1])
-    process_country(country=country)
+    process_country(country)
     print(f"Finished processing country: {country}")
