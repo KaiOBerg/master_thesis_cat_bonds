89d5e7354f4ab51694ce43b147939bca11a4cc85
KaiOBerg <96840853+KaiOBerg@users.noreply.github.com>
2024-11-26 16:42:30 +0100

    added cc module

--- country_basics_cc_euler.py
+++ country_basics_cc_euler.py
@@ -1,12 +1,15 @@
 import sys
 from pathlib import Path
-import exposure_cc_euler as ex_cc_eu
+import exposures_cc as ex_cc
+
+STORM_DIR = Path("/cluster/work/climate/kbergmueller/storm_tc_tracks/climate_change")
+OUTPUT_DIR = Path("/cluster/work/climate/kbergmueller")
 
 
 def process_country(cty, cc_model):
     """Wrapper function to process a single country."""
     print(f"Processing country: {cty}")
-    ex_cc_eu.init_TC_exp(country=cty, cc_model=cc_model, buffer_distance_km=105, res_exp=30, grid_size=6000, buffer_grid_size=1, load_fls=True, plot_exp=False, plot_centrs=False, plt_grd=False)
+    ex_cc.init_TC_exp(country=cty, cc_model=cc_model, file_path=OUTPUT_DIR, storm_path=STORM_DIR, buffer_distance_km=105, res_exp=30, grid_size=6000, buffer_grid_size=1, load_fls=True, plot_exp=False, plot_centrs=False, plt_grd=False)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
