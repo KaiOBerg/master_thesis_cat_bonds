e489608dbbc1137b4fb632097bb09f98be780732
KaiOBerg <96840853+KaiOBerg@users.noreply.github.com>
2024-12-20 17:27:10 +0100

    adjusted N

--- pooling_max_nom.py
+++ pooling_max_nom.py
@@ -30,13 +30,13 @@ nominals_sng = pd.read_csv(OUTPUT_DIR.joinpath("nominals_sng.csv"))
 max_nominal = 100000000000
 
 #set alpha for risk diversification optimization
-RT = 500
+RT = 200
 alpha = 1-1/RT 
 
-n_opt_rep = 50
+n_opt_rep = 100
 opt_rep = range(0,n_opt_rep,1)
 
-N_arr = [1,2,3]
+N_arr = [1,2,3,4,5]
 
 def process_n(n, cntry_names, df_losses, alpha, nominals_sng, max_nominal, output_file):
     fig, ax = plt.subplots(1, 1, figsize=(10,5))
@@ -98,10 +98,10 @@ def process_n(n, cntry_names, df_losses, alpha, nominals_sng, max_nominal, outpu
     ind_min_conc = df_conc.index[df_conc['Min_Concentration'] == min_conc].tolist()
     ind_min = list(set(ind_min_conc))
     df_result = df_cntry_allocation.loc[ind_min].reset_index(drop=True)
-    df_result.to_csv(output_file.joinpath(f"df_result_{n}_pools"), index=False, sep=',')
+    df_result.to_csv(output_file.joinpath(f"df_result_{n}_pools.csv"), index=False, sep=',')
     fig.savefig(output_file.joinpath(f"convergence_plot_{n}_pools.png"), dpi=300, bbox_inches='tight')
     print(f'Round {n} finished')
-    return df_result, fig
+    return df_result, fig, min_conc
 
 def main():
     # Use ThreadPoolExecutor to parallelize the outer loop (over N_arr)
@@ -111,8 +111,9 @@ def main():
             for n in N_arr
         ]
         for future in concurrent.futures.as_completed(futures):
-            result, fig = future.result()
+            result, fig, min_conc = future.result()
             print(result)
+            print("Min conc: ", min_conc)
 
 if __name__ == "__main__":
     main()
