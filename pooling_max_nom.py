ba69cbe858adba85f09def99ffeda73f42be38b1
KaiOBerg <96840853+KaiOBerg@users.noreply.github.com>
2025-01-11 17:27:36 +0100

    new countries

--- pooling_max_nom.py
+++ pooling_max_nom.py
@@ -20,12 +20,15 @@ from pymoo.operators.repair.rounding import RoundingRepair
 OUTPUT_DIR = Path("/cluster/work/climate/kbergmueller/cty_data")
 
 #choose country
-countries = [212, 332, 670, 388, 548, 242, 776, 174, 584]
+countries = [480, 212, 882, 332, 670, 388, 662, 214, 548, 242, 776, 174, 584]
+countries_str = ['480', '212', '882', '332', '670', '388', '662', '214', '548', '242', '776', '174', '584']
 
-sng_ann_losses = pd.read_csv(OUTPUT_DIR.joinpath("sng_losses_fs.csv"))
-nominals_sng_dic = pd.read_csv(OUTPUT_DIR.joinpath("nominal_dic_df_fs.csv"))
+sng_ann_losses = pd.read_csv(OUTPUT_DIR.joinpath("sng_losses.csv"))
+sng_ann_losses_filtered = sng_ann_losses[countries]
+
+nominals_sng_dic = pd.read_csv(OUTPUT_DIR.joinpath("nominal_dic_df.csv"))
 nominals_sng = nominals_sng_dic.set_index('Key').loc[countries, 'Value'].tolist()
-max_nominal = 6000000000
+max_nominal = 26000000000
 
 #set alpha for risk diversification optimization
 RT = 10000
